de16fe9e3d4ea110879bac116c6f7cdd0c29a630
Agustin Miguel <Agustin.Miguel@tsoftglobal.com>
2021-09-07 10:40:33 -0300

    fist }commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,55 @@
+import React, {useState, useRef} from 'react';
+import Card from '../UI/Card'
+import Button from '../UI/Button'
+import classes from './AddUser.module.css'
+import ErrorModal from '../UI/ErrorModal';
+
+const AddUser = (props) => {
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+    const[error, setError] = useState();
+
+    const errorHandler = () =>{
+        setError(null);
+    }
+
+    const addUserHandler = (event) => {
+        event.preventDefault();
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0){
+            setError({
+                title:'Invalid Input',
+                message: 'Please enter a valid name and age.'
+            });
+            return;
+        }
+        if(+enteredUserAge < 1){
+            setError({
+                title:'Invalid Input',
+                message: 'Please enter a valid age.'
+            });
+            return;
+        }
+        props.onAddUser(enteredName, enteredUserAge);
+        nameInputRef.current.value = ' ';
+        ageInputRef.current.value= ' ';
+    };
+
+    return(
+        <React.Fragment>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm = {errorHandler}/>}
+            <Card cssClass={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" ref={nameInputRef}/>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" ref={ageInputRef}/> 
+                    <Button type="submit"  >Add User</Button>
+                </form>
+            </Card>
+        </React.Fragment>
+    )
+}
+
+export default AddUser;
